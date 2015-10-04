8f74320e846ef3082694f36219d15950c513a7b4
Vignesh S <svignesh0308@gmail.com>
2015-10-05 01:38:40 +0530

    First Commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,51 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.Ext.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices.Ext",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.1.0",
+    "com.microsoft.azure-mobile-services": "1.2.9",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-network-information": "1.0.1",
+    "org.apache.cordova.inappbrowser": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/windows/www/cordova_plugins.js
@@ -0,0 +1,93 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.Ext.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices.Ext",
+        "runs": true
+    },
+    {
+        "file": "plugins/com.microsoft.azure-mobile-services/www/MobileServices.Web.js",
+        "id": "com.microsoft.azure-mobile-services.AzureMobileServices",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/src/windows/GeolocationProxy.js",
+        "id": "cordova-plugin-geolocation.GeolocationProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Coordinates.js",
+        "id": "cordova-plugin-geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/Position.js",
+        "id": "cordova-plugin-geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/src/windows/NetworkInfoProxy.js",
+        "id": "cordova-plugin-network-information.NetworkInfoProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/src/windows/InAppBrowserProxy.js",
+        "id": "org.apache.cordova.inappbrowser.InAppBrowserProxy",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.1.0",
+    "com.microsoft.azure-mobile-services": "1.2.9",
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-network-information": "1.0.1",
+    "org.apache.cordova.inappbrowser": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
